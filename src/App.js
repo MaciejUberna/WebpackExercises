4eac654eeeb97f7e1d8f7fc1950e1af682e4e3d0
Maciej Uberna <maciej.uberna@gmail.com>
2020-04-23 20:16:37 +0200

    Added spaces between Uzers and Pizza Links.

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
         return (
             <div>
                 <div>
-                    <Link to="/">Users</Link>
-                    <Link to="/pizza">Pizza</Link>
+                    <Link to="/">Users </Link>
+                    <Link to="/pizza"> Pizza</Link>
                 </div>
                 <div>
                     <Route path="/" exact component={Users}/>
